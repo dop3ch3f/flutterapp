45aad83418f40958d451264d43a5d0365d0e730d
dop3ch3f <ifeanyi.ibekie@gmail.com>
2019-01-12 10:38:19 +0100

    About to implement views for hacker api

---
+++ test/news_api_provider_test.dart
@@ -0,0 +1,32 @@
+import 'package:pics/src/resources/news_api_provider.dart';
+import 'dart:convert';
+import 'package:test_api/test_api.dart';
+import 'package:http/testing.dart';
+import "package:http/http.dart";
+
+void main() {
+  test('FetchTopIDs (return a list of ids)', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+
+    final ids = await newsApi.fetchTopIds();
+
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('FetchItem (returns an item model)', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {
+        'id': 123,
+      };
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItem(999);
+
+    expect(item.id, 123);
+  });
+}
